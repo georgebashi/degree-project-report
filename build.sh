ffe14cc1152c4b1cee0289a7435d56b7a1ea0f42
George Bashi <jamil@georgebashi.com>
2007-04-16 07:51:42 +0000

    * Changed -march for tablet * Added word count to build.sh * Wrote stuff! b Fixed syntax in spectral intensity b Fixed(?!) display of international chars b Fixed bug in extractor, using &> which isn't POSIX

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+if [ -z $1 ]
+then
+	find . -name '*.tex' | grep -Ev "appendicies|formulae|front" | xargs cat | grep -v "\item" > /tmp/wordcount
+elif [ "$1" = "`pwd`/report.tex" ]
+then
+	find . -name '*.tex' | grep -Ev "appendicies|formulae|front" | xargs cat | grep -v "\item" > /tmp/wordcount
+else
+	cat $1 | grep -v "\item" > /tmp/wordcount
+fi
 
-echo `svnversion .` `date "+%D %T"` > front/version.tex
+./TeXcount.pl -vvv -html /tmp/wordcount > wordcount.html
+WORDS=`./TeXcount.pl /tmp/wordcount | grep "Words in text:" | cut -c 15-`
+rm /tmp/wordcount
+
+echo `svnversion .` `date "+%D %T"` $WORDS > front/version.tex
 rubber -d report
