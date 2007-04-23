4e0ba81c2283a5c09bae2963164cfa22bf5de4d4
George Bashi <jamil@georgebashi.com>
2007-04-23 20:55:32 +0000

    THIS IS THE VERSION ALEX PROOFREAD * Everything cuts off .vec now! * Fixed CSV header in viewer * Commented code a bunch * FIXED LEARNER! * Added new fixed weights to generator * -s in generator now skips songs with NaN scores * Added dedication * Lots more writing

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -z $1 ]
 then
-	find . -name '*.tex' | grep -Ev "appendicies|formulae|front" | xargs cat | grep -v "\item" > /tmp/wordcount
+	find . -name '*.tex' | grep -Ev "appendicies|formulae|front|testing/output" | xargs cat | grep -v "\item" > /tmp/wordcount
 elif [ "$1" = "`pwd`/report.tex" ]
 then
-	find . -name '*.tex' | grep -Ev "appendicies|formulae|front" | xargs cat | grep -v "\item" > /tmp/wordcount
+	find . -name '*.tex' | grep -Ev "appendicies|formulae|front|testing/output" | xargs cat | grep -v "\item" > /tmp/wordcount
 else
 	cat $1 | grep -v "\item" > /tmp/wordcount
 fi
